4e33a73f712bc96028ceefd17aa5fcc0b2dcb5e7
Madu25ss <eduardaserpa25@gmail.com>
2024-12-16 21:59:40 -0400

    primeiroCommitPush

---
+++ Basico/01.js
@@ -0,0 +1,11 @@
+//print no console | concatenação com variáveis = `${}`
+console.log('');
+console.log("");
+console.log(``);
+
+//declarar variáveis = let, var(antigo) -> mudam de valor
+let x = 15;
+console.log(`teste ${x}`);
+//constantes = const -> não mudam de valor 
+
+
