3005acaf1a2995ba6a25f68a079788575f00a41b
Madu25ss <eduardaserpa25@gmail.com>
2024-12-18 09:41:19 -0400

    commitEmendTeste

--- Basico/01.js
+++ Basico/01.js
@@ -8,4 +8,25 @@ let x = 15;
 console.log(`teste ${x}`);
 //constantes = const -> não mudam de valor 
 
+//template String = concatenação com ``
+console.log(`variável x = ${x}`);
+
+
+/*Tipo de dados primitivos: 
+string, number(int|double|float), undefined(não aponta pra nenhum valor na memória), 
+null(propositalmente vazio),
+boolean(true|false)*/
+
+//operadores: **potenciação
+//parseint/ parsefloat -> transformar em int ou float
+let y = '3';
+console.log(parseInt(y));
+console.log(typeof y);
+
+//Funções dentro de objetos = métodos | fora do objeto = função
+//MÉTODOS: alert, confirm e prompt(recebem respostas) 
+// -> window.prompt, ... 
+
+let TestePush = 27;
+
 
