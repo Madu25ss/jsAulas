34bd987500bb233064cb694213a55f8295127f91
Madu25ss <eduardaserpa25@gmail.com>
2024-12-26 11:14:03 -0400

    commit26_12_2

--- LogicaProg/for.js
+++ LogicaProg/for.js
@@ -1,5 +1,5 @@
 //ESTRUTURAS DE REPETIÇÃO: 
-//FOR
+//FOR: geralmente com iteráveis(array ou string)
 
 //com number
 let n = 0;
@@ -17,7 +17,7 @@ for (let i=0; i< lista.length; i++) {
 
 //exerc for e html, árvore dom
 
-//FOR IN: lê os indeces ou chaves de um objeto
+//FOR IN: lê os indeces ou chaves de um objeto. retorna indeces ou chaves(string, array ou objetos)
 /*
 for (let i=0; i< lista.length; i++) {
     console.log(lista[i], `no indece: ${i}`);
@@ -42,7 +42,7 @@ for (let chaves in pessoa) {
 
 
 
-//FOR OF: 
+//FOR OF: retona o valor em si(iteráveis, array, string)
 const nomes = ['nome', 'Sobrenome'];
 
 for (let valor of nomes) {
---
+++ LogicaProg/while.js
@@ -0,0 +1,9 @@
+//ESTRUTURAS REPETITIVAS
+//while 
+
+let n = 0;
+while (n<=5) {
+    console.log(`Valor de n = ${n}`);
+    n++;
+};
+//do while
