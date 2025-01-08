8b64deacd9f238a3fe1df375bd44633941fede57
Madu25ss <eduardaserpa25@gmail.com>
2025-01-07 22:22:40 -0400

    commit07_01Pc1

--- Funcoes/FactoryConstruct.js
+++ Funcoes/FactoryConstruct.js
@@ -3,6 +3,14 @@ function criaPessoa(nome, sobrenome, a, p) {
     return {
         nome, 
         sobrenome,
+        get nomeCompleto() {
+            return `${this.nome} ${this.sobrenome}`;
+        },
+        set nomeCompleto(valor) {
+            valor = valor.split(' ');
+            this.nome = valor.shift();
+            this.sobrenome = valor.join();
+        },
         fala() {
             return `Nome: ${this.nome} // Sobrenome: ${this.sobrenome}`;
         },
@@ -22,7 +30,6 @@ function criaPessoa(nome, sobrenome, a, p) {
     };
 }
 
-//setter?
 
 const p1 = criaPessoa('nome1', 'sobrenome1', 1.8, 80);
 console.log(p1.fala());
@@ -31,6 +38,9 @@ console.log(p1.nome);
 console.log(p1.imc());
 //imc como atributo: 
 //console.log(p1.imc);
+//console.log(p1.nomeCompleto);
+p1.nomeCompleto = 'nome2 sobrenome2';
+console.log(p1.nomeCompleto);
 
 
 
@@ -40,17 +50,19 @@ console.log(p1.imc());
 
 
 //Funções construtoras: constructor functions -> cria objetos na declaração deles, retorna objetos 
-function Construtor(nome, sobrenome) {
-    //métodos privados 
-    const id = 123;
+class Construtor {
+    constructor(nome, sobrenome) {
+        //métodos privados 
+        const id = 123;
 
-    //métodos públicos
-    this.nome = nome;
-    this.sobrenome = sobrenome;
-    console.log(nome, sobrenome);
+        //métodos públicos
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        console.log(nome, sobrenome);
 
-    this.metodo = function() {
-        console.log(this.nome + ` método dentro do objeto.`);
+        this.metodo = function () {
+            console.log(this.nome + ` método dentro do objeto.`);
+        };
     }
 };
 
