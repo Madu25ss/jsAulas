16f5a7d06fa3d0d65481ea7e1a22b508ef592092
Madu25ss <eduardaserpa25@gmail.com>
2025-03-06 21:31:45 -0400

    commit06_03Pc01

--- Objetos_Prototypes/polimorfismo.js
+++ Objetos_Prototypes/polimorfismo.js
@@ -1,43 +1,74 @@
 //Polimorfismo: criando uma superClasse conta que implementa classe Conta corrente e Conta poupança
+//js faz polimorfismo de reescrita, onde é possível sobrescrever os métodos e funções.
 
 //Agencia, conta e saldo
 
-//SuperClasse Conta -> Construtor -> base
+//SuperClass
 function Conta(agencia, conta, saldo) {
     this.agencia = agencia;
     this.conta = conta;
     this.saldo = saldo;
+
+
 }
 
 Conta.prototype.sacar = function(valor){
-    if (this.saldo >= valor) {
-        this.saldo -= valor;
-    }else {
-        console.log(`Saldo Insuficiente`);
-        this.verSaldo();
-    }
+    if (this.saldo < valor) {
+        console.log(`Saldo Insuficiente: ${this.saldo}`)
+        return;
+    };
+    
+    this.saldo -= valor; 
+    this.verSaldo();   
 };
-
 Conta.prototype.depositar = function(valor){
     this.saldo += valor;
+    this.verSaldo();
 };
-
 Conta.prototype.verSaldo = function(){
-    console.log(`Ag/c: ${this.agencia}/${this.conta} | Saldo = R$${this.saldo.toFixed(2)}`);
+    console.log(`Ag/c: ${this.agencia}/${this.conta} | ` + 
+    `Saldo: R$${this.saldo.toFixed(2)}`
+    );
 };
 
-const conta01 = new Conta('0123', '4567', 100);
-console.log(conta01);
-conta01.sacar(101);
-conta01.depositar(20);
-conta01.sacar(10);
-conta01.verSaldo();
+const conta1 = new Conta(11, 22, 10);
+console.log(conta1);
+conta1.depositar(11);
+conta1.sacar(20);
 
 
-function ContaCorrente(agencia, conta, saldo, limite) {
+function CC(agencia, conta, saldo, limite) {
     Conta.call(this, agencia, conta, saldo);
     this.limite = limite;
+
 }
 
-ContaCorrente.prototype = Object.create(Conta.prototype);
-ContaCorrente.prototype.contructor = ContaCorrente;
+CC.prototype = Object.create(Conta.prototype);
+CC.prototype.constructor = CC;
+
+CC.prototype.sacar = function(valor) {
+    if (valor > (this.saldo + this.limite)) {
+        console.log(`Saldo Insuficiente: ${this.saldo}`);
+        return;
+    }
+
+    this.saldo -= valor;
+    this.verSaldo();
+}
+
+const contaCorrente01 = new CC(11, 22, 0, 100);
+contaCorrente01.depositar(10);
+contaCorrente01.sacar(90);
+
+function CP(agencia, conta, saldo, limite) {
+    Conta.call(this, agencia, conta, saldo);
+    this.limite = limite;
+
+}
+
+CP.prototype = Object.create(Conta.prototype);
+CP.prototype.constructor = CP;
+
+const cp = new CP(12, 33, 0);
+
+
